82678f46b41fde273124531169b831ede47f8ba8
Jorge Benavides <jorge.benavides@facturatech.co>
2023-01-12 14:24:40 -0600

    Mails

---
+++ resources/views/emails/notificacion.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="x-apple-disable-message-reformatting">
+    <title>Confirmación</title>
+</head>
+
+<div style="font-family: Arial,sans-serif;width:100%;">
+    
+    <div style="width: 800px; background-color: #FFFFFF; margin: 0 auto;">
+        <div>
+            <div>
+                <img src="{{asset('/img/textura.png')}}" alt="Textura" style="width:100%;">
+            </div>
+            
+            <div style="height: 230px; width:100%; display:table; margin-top:-75px;position:relative;z-index:1;">
+                <div style="display:table-cell; vertical-align:middle; text-align:center;">
+                        <div>
+                            <span style="font-size:40px">CONFIRMACIÓN</span>
+                        </div>
+                        <div>
+                            <span style="font-size:52px;line-height:35px"><b>DE PEDIDO</b></span>
+                        </div>
+                        <div style="padding-top:10px;">
+                            <span class="font-size:20px;"><b>¡Se ha realizado una compra!</b></span>
+                        </div>
+                        <div>
+                            <span style="font-size:17px;">A continuación le mostramos los detalles</span>
+                        </div>
+                    
+                </div>
+                <div style="position:absolute; z-index:-1; right:27%; top: 15px;">
+                    <img src="{{asset('/img/signo.png')}}" alt="Signo" style="width:50px;">
+                </div>
+            </div>
+        </div>
+        <div style="margin: 0px 15px;">
+            <table style="border-spacing: 0;">
+                <tr>
+                    <td><h2 style="font-size:20px;color:#ff4b71;">DETALLES DEL USUARIO</h2></td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Nombre completo o Razón social: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>NIT: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                    
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Email: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Teléfono: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div style="border-bottom:1px solid #ffd066;margin-top:15px"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td><h2 style="font-size:20px;color:#ff4b71;">DETALLES DEL PEDIDO</h2></td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>N° de pedido: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Paquete: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Número de folio: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Forma de pago: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Monto: </b></span><span style="color: #062a51;font-size: 16px;">XXXXXXXXXXX</span>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td colspan="2" nota="padding-top:15px;">
+                        <span style="color: #FF4B71; font-size: 15px;"><b>NOTA: </b></span><span style="color: #062a51;font-size: 14px; text-align: justify;">Si tu pago será a través del método de CONSIGNACIÓN, deberás tener presente que el banco probablemente genere un cobro de comisión, para esto, deberás asumir el mismo.</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <div style="border-bottom:1px solid #ffd066;margin-top:15px"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td><h2 style="font-size:20px;color:#ff4b71;">DATOS BANCARIOS</h2></td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Razón social: </b></span><span style="color: #062a51;font-size: 16px;">Ftech Colombia S.A.S</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>NIT: </b></span><span style="color: #062a51;font-size: 16px;">901143311</span>
+                    </td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Banco: </b></span><span style="color: #062a51;font-size: 16px;">Ahorros Bancolombia</span>
+                    </td>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>N° de cuenta: </b></span><span style="color: #062a51;font-size: 16px;">907 878 923 44</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span style="font-size: 18px; color: #062a51;"><b>Referencia alfanumérica: </b></span><span style="color: #062a51;font-size: 16px;">TECH25102213589033</span>
+                    </td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td colspan="2" style="padding-top:15px;">
+                        <span style="color: #FF4B71; font-size: 15px;"><b>NOTA: </b></span><span style="color: #062a51;font-size: 14px; text-align: justify;">Si deseas factura, asegúrate de tener capturados todos tus datos fiscales, para verificarlo accede a tu plataforma en la sección <a style="color: #062a51;" href="#"><b>"Mis Datos"</b></a></span>
+                    </td>
+                </tr>
+                <tr style="line-height: 30px;">
+                    <td colspan="2">
+                        <div style="padding-top: 30px; padding-bottom: 30px;text-align:center;">
+                            <div style="width:500px;margin:0 auto;padding-top: 5px; padding-bottom: 5px;background-color:#ffd066;cursor:pointer">
+                                <div style="color:#062a51">ADJUNTA TU COMPROBANTE DE PAGO <b>AQUÍ</b></div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+    </div>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/* Route::get('/mail', function () {
+    return view('emails.notificacion');
+});
+Route::get('enviar', ['as' => 'enviar', function () {
+    $data = ['link' => 'http://styde.net'];
+    \Mail::send('emails.notificacion', $data, function ($message) {
+        $message->from('email@styde.net', 'Styde.Net');
+        $message->to('jorge.benavides@facturatech.co')->subject('Confirmación');
+    });
+    return "Se envío el email";
+}]);
+Route::get('enviar2', ['as' => 'enviar', function () {
+    $data = ['link' => 'http://styde.net'];
+    \Mail::send('emails.test', $data, function ($message) {
+        $message->from('email@styde.net', 'Styde.Net');
+        $message->to('jorge.benavides@facturatech.co')->subject('Test');
+    });
+    return "Se envío el email";
+}]); */
